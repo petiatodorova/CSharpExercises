2784259b56a6454497206eaa8dab81cf600c73ef
Petya Todorova <invisioncl@gmail.com>
2018-03-09 22:24:29 +0200

    Added a Visual Studio project.

---
+++ TheProject/TheWnNumber/TheWinNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TheWnNumber
+{
+    class TheWinNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess the number:");
+            while (true)
+            {
+                Int32.TryParse(Console.ReadLine(), out int n);
+                if (n == 5)
+                {
+                    Console.WriteLine($"Win! Your code number is {n + 5}!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"I need the number. Try again:");
+                }
+            }
+
+            Console.WriteLine($"The program has finished.");
+
+        }
+    }
+}
