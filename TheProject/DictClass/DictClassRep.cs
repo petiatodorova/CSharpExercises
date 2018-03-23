7a49d376325b837eea39f0890b9b0b06d2aed64d
Petya Todorova <invisioncl@gmail.com>
2018-03-23 14:36:09 +0200

    Added DictClass/DictClassRep.cs and DictClassNoRep/DictClassNoRep.cs

---
+++ TheProject/DictClass/DictClassRep.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DictClassNoRep
+{
+    class DictClassRep
+    {
+        static void Main(string[] args)
+        {
+            //с повтарящи се имена за различните цветове
+
+            //Ivan apple red / 32 14.4
+            //Slav; kiwi blue 17 33.10
+            //Slav grape blue 18 , 12.4
+            //Grigor apple red / 77 14.4
+            //Mimi portocal blue  \ 19 18.8
+            //Koni grape yellow / 33   17.6
+            //Mihail orange yellow 33   18.20
+            //End
+
+            Dictionary<string, List<Customr>> customers = new Dictionary<string, List<Customr>>(); // color, Customr
+            string newInput = Console.ReadLine();
+            
+            while (true)
+            {
+                if (newInput == "End")
+                {
+                    break;
+                }
+                Customr cust = ReadCustomr(newInput);
+                string color = cust.Color;
+                if (!customers.ContainsKey(color)) //ако няма такъв цвят
+                {
+                    List<Customr> curCust = new List<Customr>();
+                    curCust.Add(cust);
+                    customers.Add(color, curCust);
+                }
+                else  //ако има такъв цвят, но няма такова име
+                {
+                    customers[color].Add(cust);
+                }
+                newInput = Console.ReadLine();
+            }
+
+            foreach (var cust in customers)
+            {
+                string theColor = cust.Key;
+                Console.WriteLine($"The Color: --> {theColor}");
+                List<Customr> theValues = cust.Value;
+                foreach (var person in theValues.OrderBy(x => x.Name))
+                {
+                    Console.WriteLine($"Name: {person.Name} --> Total Money: {person.TotalMoney}");
+                }
+            }
+        }
+
+        static Customr ReadCustomr(string input)
+        {
+            string[] inputArr = input
+                .Split(new char[] {' ','/','\\',';',',' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            return new Customr
+            {
+                Name = inputArr[0],
+                Fruit = inputArr[1],
+                Color = inputArr[2],
+                Quantity = double.Parse(inputArr[3]),
+                Price = decimal.Parse(inputArr[4])
+            };
+        }
+    }
+
+    class Customr
+    {
+        public string Name { get; set; }
+        public string Fruit { get; set; }
+        public string Color { get; set; }
+        public double Quantity { get; set; }
+        public decimal Price { get; set; }
+
+        public decimal TotalMoney
+        {
+            get
+            {
+                return (decimal)Quantity * Price;
+            }
+        }
+
+    }
+}
---
+++ TheProject/DictClassNoRep/DictClassNoRep.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DictClassNoRep
+{
+    class DictClassNoRep
+    {
+        static void Main(string[] args)
+        {
+            //за повтарящи се имена за различните цветове добавя тотала към предния
+
+            //Ivan apple red / 32 14.4
+            //Slav; kiwi blue 17 33.10
+            //Slav grape blue 18 , 12.4
+            //Grigor apple red / 77 14.4
+            //Mimi portocal blue  \ 19 18.8
+            //Koni grape yellow / 33   17.6
+            //Mihail orange yellow 33   18.20
+            //End
+
+            string input = Console.ReadLine();
+            //Dictionary<color, Dict<name, totalmoney>>
+            //
+            Dictionary<string, Dictionary<string, decimal>> users = new Dictionary<string, Dictionary<string, decimal>>();
+
+            while (true)
+            {
+                if (input == "End")
+                {
+                    break;
+                }
+                User curUser = ReadUser(input);
+                string color = curUser.Color;
+                string name = curUser.Name;
+                decimal money = curUser.TotalMoney;
+
+                if (!users.ContainsKey(color))
+                {
+                    Dictionary<string, decimal> currentNameMoney = new Dictionary<string, decimal>();
+                    currentNameMoney.Add(name, money);
+                    users.Add(color, currentNameMoney);
+                }
+                else if (!users[color].ContainsKey(name))
+                {
+                    users[color].Add(name, money);
+                }
+                else
+                {
+                    users[color][name] += money;
+                }
+                input = Console.ReadLine();
+            }
+
+            foreach (var user in users.OrderBy(x => x.Key))
+            {
+                string theColor = user.Key;
+                Dictionary<string, decimal> theNamesMoney = user.Value;
+                Console.WriteLine($"The Color is: ---> {theColor}");
+                foreach (var nameMoney in theNamesMoney.OrderBy(x => x.Value))
+                {
+                    Console.WriteLine($"Name is: {nameMoney.Key} Money are: {nameMoney.Value}");
+                }
+            }
+        }
+
+        static User ReadUser(string input)
+        {
+            string[] arrInput = input
+                .Split(new char[] {' ',',','/','\\',';' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            return new User
+            {
+                Name = arrInput[0],
+                Fruit = arrInput[1],
+                Color = arrInput[2],
+                Quantity = decimal.Parse(arrInput[3]),
+                Price = decimal.Parse(arrInput[4])
+            };
+        }
+    }
+
+    class User
+    {
+        public string Name { get; set; }
+        public string Fruit { get; set; }
+        public string Color { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Price { get; set; }
+
+        public decimal TotalMoney
+        {
+            get
+            {
+                return Quantity * Price;
+            }
+            set
+            {
+            }
+        }
+    }
+}
